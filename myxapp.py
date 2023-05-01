5563837711133dc0de0723330523e699b1140457
Eugenio Moro <eugenio.moro@polimi.it>
2023-05-01 20:28:10 +0000

    working xapp example

--- myxapp.py
+++ myxapp.py
@@ -1,13 +1,56 @@
-import src.e2ap_xapp as hwxappp
+import src.e2ap_xapp as e2ap_xapp
+from ran_messages_pb2 import *
+from time import sleep
+from ricxappframe.e2ap.asn1 import IndicationMsg
 
+def xappLogic():
 
-def launchXapp():
-    hwxapp = hwxappp.HWXapp()
-    hwxapp.start()
+    # instanciate xapp 
+    connector = e2ap_xapp.e2apXapp()
 
+    # get gnbs connected to RIC
+    gnb_id_list = connector.get_gnb_id_list()
+    print("{} gNB connected to RIC, listing:".format(len(gnb_id_list)))
+    for gnb_id in gnb_id_list:
+        print(gnb_id)
+    print("---------")
+
+    # subscription requests
+    for gnb in gnb_id_list:
+        e2sm_buffer = e2sm_report_request_buffer()
+        connector.send_e2ap_sub_request(e2sm_buffer,gnb)
+    
+    # read loop
+    sleep_time = 4
+    while True:
+        print("Sleeping {}s...".format(sleep_time))
+        sleep(sleep_time)
+        messgs = connector.get_queued_rx_message()
+        if len(messgs) == 0:
+            print("{} messages received while waiting".format(len(messgs)))
+            print("____")
+        else:
+            print("{} messages received while waiting, printing:".format(len(messgs)))
+            for msg in messgs:
+                if msg["message type"] == connector.RIC_IND_RMR_ID:
+                    print("RIC Indication received from gNB {}, decoding E2SM payload".format(msg["meid"]))
+                    indm = IndicationMsg()
+                    indm.decode(msg["payload"])
+                    resp = RAN_indication_response()
+                    resp.ParseFromString(indm.indication_message)
+                    print(resp)
+                    print("___")
+                else:
+                    print("Unrecognized E2AP message received from gNB {}".format(msg["meid"]))
+
+def e2sm_report_request_buffer():
+    master_mess = RAN_message()
+    master_mess.msg_type = RAN_message_type.INDICATION_REQUEST
+    inner_mess = RAN_indication_request()
+    inner_mess.target_params.extend([RAN_parameter.GNB_ID, RAN_parameter.UE_LIST])
+    master_mess.ran_indication_request.CopyFrom(inner_mess)
+    buf = master_mess.SerializeToString()
+    return buf
 
 if __name__ == "__main__":
-
-    print(__package__)
-    print("pd")
-    launchXapp()
+    xappLogic()
--- src/e2ap_xapp.py
+++ src/e2ap_xapp.py
@@ -19,7 +19,7 @@
 from os import getenv
 from ricxappframe.xapp_frame import rmr, Xapp
 
-from ricxappframe.e2ap.asn1 import IndicationMsg, SubResponseMsg, SubRequestMsg, ControlRequestMsg, ActionDefinition, SubsequentAction, ARRAY, c_uint8
+from ricxappframe.e2ap.asn1 import IndicationMsg, SubRequestMsg, ControlRequestMsg, ActionDefinition, SubsequentAction
 from ricxappframe.entities.rnib.nb_identity_pb2 import NbIdentity
 
 from  ran_messages_pb2 import *
@@ -28,18 +28,30 @@ from time import sleep
 
 class e2apXapp:
 
-    def __init__(self,logic):
+    SUB_REQ_RMR_ID = 12040
+    RIC_IND_RMR_ID = 12050
+
+    def __init__(self):
         fake_sdl = getenv("USE_FAKE_SDL", False)
-        self._rmr_xapp = Xapp(rmr_port=4560,
+        self.engine = Xapp(rmr_port=4560,
                           rmr_wait_for_ready=True,
                           use_fake_sdl=False,
-                          entrypoint=logic)
+                          entrypoint=self.logic)
 
-    def get_gnb_list() -> list(NbIdentity):
-        
+    def _get_gnb_list(self):
+        return self.engine.get_list_gnb_ids()
     
-    @staticmethod
-    def _rmr_send_w_meid(rmr_xapp, payload, mtype, meid, retries=100):
+    def get_gnb_id_list(self):
+        """
+        Returns a list containing the gnb ids as ascii strings. Each gnb id can be safely consumed by sender functions
+        """
+        gnbids = list()
+        for gnb in self._get_gnb_list():
+            print(gnb)
+            gnbids.append(gnb.inventory_name)
+        return gnbids
+    
+    def _rmr_send_w_meid(self, payload, mtype, meid, retries=100):
         """
         Allocates a buffer, sets payload and mtype, and sends
 
@@ -57,24 +69,24 @@ class e2apXapp:
         bool
             whether or not the send worked after retries attempts
         """
-        sbuf = rmr.rmr_alloc_msg(vctx=rmr_xapp._mrc, size=len(payload), payload=payload, gen_transaction_id=True,
+        sbuf = rmr.rmr_alloc_msg(vctx=self.engine._mrc, size=len(payload), payload=payload, gen_transaction_id=True,
                                  mtype=mtype, meid=meid)
 
         for _ in range(retries):
-            sbuf = rmr.rmr_send_msg(rmr_xapp._mrc, sbuf)
+            sbuf = rmr.rmr_send_msg(self.engine._mrc, sbuf)
             if sbuf.contents.state == 0:
-                rmr_xapp.rmr_free(sbuf)
+                self.engine.rmr_free(sbuf)
                 return True
 
-        rmr_xapp.rmr_free(sbuf)
+        self.engine.rmr_free(sbuf)
         return False
 
-    def _post_init(self, rmr_xapp):
+    def logic(self):
         """
         Function that runs when xapp initialization is complete
         """
-        print("this is the post init")
-        rmr_xapp.logger.info("HWXapp.post_init :: post_init called")
+        return # I don't want to execute the following code
+    
         # self.sdl_alarm_mgr = SdlAlarmManager()
  #       sdl_mgr = SdlManager(rmr_xapp)
         #sdl_mgr.sdlGetGnbList()
@@ -85,7 +97,7 @@ class e2apXapp:
         #for enb in enb_list:
         #    sub_mgr.send_subscription_request(enb)
         #gnb_list = sub_mgr.get_gnb_list()
-        gnb_list = self._rmr_xapp.get_list_gnb_ids()
+        gnb_list = self.get_gnb_list() #self._rmr_xapp.get_list_gnb_ids()
         print(gnb_list)
         #for gnb in gnb_list:
         #    sub_mgr.send_subscription_request(gnb)
@@ -94,8 +106,8 @@ class e2apXapp:
 
         #msgbuf = self.dummy_control_request()
         #msgbuf = self.e2ap_control_request(self.e2sm_dummy_control_buffer())
-        msgbuf = self.e2ap_sub_request(self.e2sm_dummy_control_buffer())
-        self._rmr_send_w_meid(rmr_xapp,msgbuf,12040, bytes(gnb_list[0].inventory_name, 'ascii'))
+        msgbuf = self.send_e2ap_sub_request(self.e2sm_dummy_control_buffer())
+        self._rmr_send_w_meid(self,msgbuf,12040, bytes(gnb_list[0].inventory_name, 'ascii'))
         print("Waiting 5 seconds before starting printing messages")
         sleep(5)
         while True:
@@ -113,6 +125,13 @@ class e2apXapp:
                 print("_____________")
             sleep(2)
         #rmr_xapp.rmr_send()
+    
+    def get_queued_rx_message(self):
+        queued_msg = list()
+        for (summary,sbuf) in self.engine.rmr_get_messages():
+            queued_msg.append(summary)
+            rmr.rmr_free_msg(sbuf)
+        return queued_msg
 
     @staticmethod
     def e2sm_dummy_control_buffer():
@@ -152,8 +171,19 @@ class e2apXapp:
         print("control request encoded {} bytes".format(lencc))
         return bytescc
 
-    @staticmethod
-    def e2ap_sub_request(payload):
+
+    def send_e2ap_sub_request(self,payload, gnb_id):
+
+        """
+        Send an E2AP Subscription request 
+        
+        Parameters
+        ----------
+        payload: bytes
+            E2SM payload injected into event trigger definition
+        gnb_id: string
+            ASCII string containing the recepient gNB id
+        """
 
         action_definitions = list()
 
@@ -173,11 +203,11 @@ class e2apXapp:
 
         sub_request = SubRequestMsg()
         try:
-            [lencc, bytescc] = sub_request.encode(24, 1, 0, payload, [1], [1],
+            [_, bytescc] = sub_request.encode(24, 1, 0, payload, [1], [1],
                             action_definitions, subsequent_actions)
         except BaseException:
             assert False
-        return bytescc
+        self._rmr_send_w_meid(bytescc,self.SUB_REQ_RMR_ID, bytes(gnb_id, 'ascii'))
 
     def dummy_control_request():
         action_definitions = list()
@@ -212,13 +242,13 @@ class e2apXapp:
         """
         #self.createHandlers()
         #self._rmr_xapp.run(thread)
-        self._rmr_xapp.run()
+        self.engine.run()
 
     def stop(self):
         """
         can only be called if thread=True when started
         TODO: could we register a signal handler for Docker SIGTERM that calls this?
         """
-        self._rmr_xapp.stop()
+        self.engine.stop()
 
     
